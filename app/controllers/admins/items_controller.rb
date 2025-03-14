25faaeb71f0b489da1e5a9e82ba89f990dbf2306
koikeasuka <akoike@big-gate.co.jp>
2025-03-14 23:06:30 +0900

    Modify: 画像の名前変更

--- app/controllers/admins/items_controller.rb
+++ app/controllers/admins/items_controller.rb
@@ -45,6 +45,6 @@ class Admins::ItemsController < Admins::ApplicationController
   end
 
   def item_params
-    params.require(:item).permit(:name, :description, :price)
+    params.require(:item).permit(:name, :description, :price, :product_photo)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,8 @@
 class Item < ApplicationRecord
-  has_one_attached :image, resize_to_limit: [100, 100]
+  has_one_attached :product_photo do |attachable|
+    attachable.variant :thumb, resize_to_limit: [100, 100]
+    attachable.variant :image, resize_to_fit: [300, 300]
+  end
 
   validates :name, presence: true
   validates :description, presence: true
