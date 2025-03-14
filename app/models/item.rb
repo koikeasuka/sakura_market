38ee6fd7d06fda6b7a851dd4283aa9e03d497b7d
koikeasuka <akoike@big-gate.co.jp>
2025-03-15 00:54:21 +0900

    Add: itemモデルにact_as_list追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,7 @@ class Item < ApplicationRecord
     attachable.variant :thumb, resize_to_limit: [100, 100]
     attachable.variant :image, resize_to_fit: [300, 300]
   end
+  acts_as_list
 
   validates :name, presence: true
   validates :description, presence: true
