f570b8837392d4a7d827f29296299b921b379846
koikeasuka <akoike@big-gate.co.jp>
2025-03-14 22:24:20 +0900

    Add: itemモデルに画像追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
+  has_one_attached :image, resize_to_limit: [100, 100]
+
   validates :name, presence: true
   validates :description, presence: true
   validates :price, presence: true
