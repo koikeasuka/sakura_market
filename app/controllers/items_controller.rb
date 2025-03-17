eb6ac530689639145485f9d6f2aade012ea88b2a
koikeasuka <akoike@big-gate.co.jp>
2025-03-17 23:12:26 +0900

    Add: ページネーション追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.order(position: :asc).with_attached_product_photo
+    @items = Item.order(position: :asc).page(params[:page]).with_attached_product_photo
   end
 end
