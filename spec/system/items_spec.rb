7ec9d29f83773b89665e6f7b173f4351762cc30e
koikeasuka <akoike@big-gate.co.jp>
2025-03-18 00:01:58 +0900

    Add: 商品詳細のテスト追加

--- spec/system/items_spec.rb
+++ spec/system/items_spec.rb
@@ -16,4 +16,16 @@ RSpec.describe '商品画面', type: :system do
       expect(page).to have_content '300円'
     end
   end
+
+  describe '詳細' do
+    let(:item) { create(:item, name: 'さば', price: 200, description: '三陸海岸から直送で届いたサバをで缶詰にしました') }
+
+    it '商品の詳細が表示されること' do
+      visit item_path(item)
+
+      expect(page).to have_content 'さば'
+      expect(page).to have_content '200円'
+      expect(page).to have_content '三陸海岸から直送で届いたサバをで缶詰にしました'
+    end
+  end
 end
