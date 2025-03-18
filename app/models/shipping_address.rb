be006d67e44614dcd7365440a42eacf8abdc1c35
koikeasuka <akoike@big-gate.co.jp>
2025-03-19 01:25:11 +0900

    Add: shipping_addressモデル追加

---
+++ app/models/shipping_address.rb
@@ -0,0 +1,12 @@
+class ShppingAddress < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+  belongs_to :user
+
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :tel, presence: true
+  validates :post_code, presence: true, format: { with: /\A\d{3}[-]?\d{4}\z/ }
+  validates :city, presence: true
+  validates :other_address, presence: true
+end
