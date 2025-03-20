635511c9267cd1403bcb4fae73edcb3a9cae952e
koikeasuka <akoike@big-gate.co.jp>
2025-03-21 01:10:30 +0900

    Add: カート内合計金額表示

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,9 @@
 class Cart < ApplicationRecord
   belongs_to :user
   has_many :cart_items, dependent: :destroy
+  has_many :items, through: :cart_items
+
+  def total_price
+    items.sum(&:price)
+  end
 end
