ae09b7a84f2f740c8dbf267264c242188e49499c
koikeasuka <akoike@big-gate.co.jp>
2025-03-20 14:59:25 +0900

    Modify: ja.ymlにshipping_address追加

--- db/migrate/20250318160808_create_shipping_addresses.rb
+++ db/migrate/20250318160808_create_shipping_addresses.rb
@@ -1,9 +1,10 @@
 class CreateShippingAddresses < ActiveRecord::Migration[8.0]
   def change
     create_table :shipping_addresses do |t|
+      t.bigint :user_id, null: false, index: true
       t.string :last_name, null: false, default: ''
       t.string :first_name, null: false, default: ''
-      t.integer :tel, null: false, default: 0
+      t.string :tel, null: false, default: ''
       t.string :post_code, null: false, default: ''
       t.bigint :prefecture_id, null: false
       t.string :city, null: false, default: ''
--- db/schema.rb
+++ db/schema.rb
@@ -60,6 +60,20 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_18_160808) do
     t.integer "position", default: 1, null: false
   end
 
+  create_table "shipping_addresses", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "last_name", default: "", null: false
+    t.string "first_name", default: "", null: false
+    t.string "tel", default: "", null: false
+    t.string "post_code", default: "", null: false
+    t.bigint "prefecture_id", null: false
+    t.string "city", default: "", null: false
+    t.string "other_address", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_shipping_addresses_on_user_id"
+  end
+
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -75,18 +89,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_18_160808) do
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-  create_table "shipping_addresses", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "last_name", default: "", null: false
-    t.string "first_name", default: "", null: false
-    t.integer "tel", default: 0, null: false
-    t.string "post_code", default: "", null: false
-    t.bigint "prefecture_id", null: false
-    t.string "city", default: "", null: false
-    t.string "other_address", default: "", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
