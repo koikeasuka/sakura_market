6df42fbb4d871559cbc23547b5768379a786d862
koikeasuka <akoike@big-gate.co.jp>
2025-03-21 15:05:59 +0900

    Modify: カート追加失敗するケースが基本的に想定されないため、save!に変更

--- app/controllers/users/cart_items_controller.rb
+++ app/controllers/users/cart_items_controller.rb
@@ -1,11 +1,8 @@
 class Users::CartItemsController < Users::ApplicationController
   def create
     cart_item = current_cart.cart_items.build(cart_item_params)
-    if cart_item.save
-      redirect_to items_path, notice: 'カートに追加しました'
-    else
-      redirect_to items_path, notice: 'カートに追加できませんでした'
-    end
+    cart_item.save!
+    redirect_to items_path, notice: 'カートに追加しました'
   end
 
   def destroy
