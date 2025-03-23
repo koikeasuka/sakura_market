96a2dafd3a4ef65189fb55f1a1bc3f74877bec24
koikeasuka <akoike@big-gate.co.jp>
2025-03-24 01:46:14 +0900

    Modify: ページネーションをkaminariからpagyに変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.where(is_published: true).order(position: :asc).page(params[:page]).with_attached_product_photo
+    @pagy, @items = pagy(Item.where(is_published: true).order(position: :asc).with_attached_product_photo)
   end
 
   def show
